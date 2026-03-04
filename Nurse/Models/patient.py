178d76839928ec0afdb3be9f5ba69407083932d5
angvela <angelamusamali05@gmali.com>
2026-03-04 06:50:09 +0300

    added comments to the nures module

--- Nurse/Models/patient.py
+++ Nurse/Models/patient.py
@@ -1,11 +1,12 @@
 class Patient:
+    #Initalizes instance attributes
     def __init__(self, patient_id, name, age):
         self.patient_id = patient_id
         self.name = name
         self.age = age
         self.temperature = None
         self.blood_pressure = None
-
+    #patient class method for displying patient records
     def display_info(self):
         print("Patient ID:", self.patient_id)
         print("Name:", self.name)
